54d3a352b2c57be0d1424fd1f7a39824baf7dd76
cheatteam1994-ship-it <cheat.team1994@gmail.com>
2025-09-15 10:23:03 +0200

    Update app.js

--- rplace_render/frontend/app.js
+++ rplace_render/frontend/app.js
@@ -9,10 +9,6 @@
   const COLORS = ['#ffffff','#c0c0c0','#808080','#000000','#ff0000','#800000','#ffff00','#808000','#00ff00','#008000','#00ffff','#008080','#0000ff','#000080','#ff00ff','#800080','#ffa500','#a52a2a'];
   const CELL = 6;
 
-  // Canvas responsivo
-  board.width = window.innerWidth * 0.95;
-  board.height = window.innerHeight * 0.85;
-
   let canvasW = 1000;
   let canvasH = 600;
   let scale = 1, targetScale = 1;

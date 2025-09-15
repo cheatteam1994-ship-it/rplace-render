d2c8f1ef4c34c4f1d122b0e3ec09f8ab42b9129e
cheatteam1994-ship-it <cheat.team1994@gmail.com>
2025-09-15 15:53:50 +0200

    Update app.js

--- rplace_render/frontend/app.js
+++ rplace_render/frontend/app.js
@@ -23,7 +23,7 @@ let selectedColor = 1;
 let ws;
 
 // Palette colori
-const palette = [
+const palette = 
   ['#ffffff','#c0c0c0','#808080','#000000','#ff0000','#800000','#ffff00','#808000','#00ff00','#008000','#00ffff','#008080','#0000ff','#000080','#ff00ff','#800080','#ffa500','#a52a2a'];
 
 function init() {
